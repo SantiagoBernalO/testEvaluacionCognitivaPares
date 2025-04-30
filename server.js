a657b1298d82e17d5d6824e72cccab93eaad00bf
SantiagoBernalo <asantibo129@gmail.com>
2025-04-30 13:05:37 -0500

    Initial commit (sin archivos sensibles)

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./firebase');  // Configuración de Firebase Admin SDK
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+app.use(express.json());
+//app.use(express.static('public')); //express
+
+// Guardar datos en Firestore
+app.post('/guardar', async (req, res) => {
+  const data = req.body;
+  const fecha = new Date().toISOString();
+
+  try {
+    const docRef = await db.collection('resultados').add({
+      nombre: data.nombre,
+      edad: data.edad,
+      genero: data.genero,
+      educacion: data.educacion,
+      fecha: fecha,
+      cantidad_pares: data.cantidad_pares,
+      tiempo_total_segundos: data.tiempo_total_segundos,
+      intentos_totales: data.intentos_totales,
+      aciertos_totales: data.aciertos_totales,
+      errores_totales: data.errores_totales,
+      pares_presentados: data.pares_presentados || [],
+      detalle_aciertos: data.detalle_aciertos || [],
+      detalle_errores: data.detalle_errores || [],
+      detalle_intentos: data.detalle_intentos || []
+    });
+
+    res.send('Guardado correctamente en Firebase');
+  } catch (error) {
+    console.error('❌ Error al guardar en Firebase:', error);
+    res.status(500).send('Error al guardar en Firebase');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor escuchando en http://localhost:${PORT}`);
+});
